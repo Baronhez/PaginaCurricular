23c66fd3d2abd50c25a481d43334322a792623db
Jonathan Ródenas López <57847247+Baronhez@users.noreply.github.com>
2021-02-17 23:25:18 +0100

    Upgrade up button

--- main.js
+++ main.js
@@ -13,29 +13,10 @@ ScrollReveal().reveal('.footercontainer', { delay: 250 });
 ScrollReveal().reveal('.form', { delay: 250 });
 ScrollReveal().reveal('.footer', { delay: 250 });
 
-// When the user scrolls down 20px from the top of the document, show the button.
-window.onscroll = function() {scrollFunction()};
+// 
 
-function scrollFunction() {
-    var go_up = document.getElementById("myBtn");
-  if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 650) {
-    go_up.style.display = "block";
-  } else {
-    go_up.style.display = "none";
-  }
-}
-$(document).ready(function(){
- 
-$(document).scroll(function() {
-  var y = $(this).scrollTop();
-  var go_up = document.getElementById("myBtn");
-  if (y > 800) {
-    $('.elementToFadeInAndOut').fadeIn();
-  } else {
-    $('.elementToFadeInAndOut').fadeOut();
-  }
-}); 
-});
+
+
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
